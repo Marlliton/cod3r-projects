49b52c4e65293f9937e8131c2e2ab706f6052162
Marlliton <marlliton.souza1@gmail.com>
2022-08-02 10:58:49 -0300

    finalizado

--- projetos-de-artigos/maquina-de-escrever/src/pages/index.jsx
+++ projetos-de-artigos/maquina-de-escrever/src/pages/index.jsx
@@ -2,10 +2,19 @@ import { useEffect } from "react";
 import { MaquinaDeEscrever } from "../components/MaquinaDeEscrever";
 
 export default function Home() {
-
   return (
     <div className="bg-[#080713] flex justify-center items-center h-screen w-full text-2xl">
-      <MaquinaDeEscrever text="Testando o componente, Máquina De Escrever" />
+
+      <h1 className="font-bold">
+        <MaquinaDeEscrever text="Olá eu sou" esconderCursor />
+      
+        <span className="text-3xl">
+          <MaquinaDeEscrever text="Marlliton Souza" delay={1300} esconderCursor />
+        </span>
+        
+        <MaquinaDeEscrever text="Front-end Developer" delay={2800} />
+      </h1>
+
     </div>
   );
 }
--- projetos-de-artigos/maquina-de-escrever/tailwind.config.js
+++ projetos-de-artigos/maquina-de-escrever/tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "animar-cursor": {
+          "0%": {
+            color: "rgb(124 58 237)",
+          },
+          "100%": {
+            color: "rgba(0,0,0,0)",
+          },
+        },
+      },
+      animation: {
+        "animar-cursor": "animar-cursor .8s infinite",
+      },
+    },
   },
   plugins: [],
-}
+};
