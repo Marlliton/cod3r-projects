de5ef3b943b74ada4206e810bdb51702cec209b3
Marlliton Souza <67978032+Marlliton@users.noreply.github.com>
2022-11-10 15:59:24 -0300

    Estruturando projeto

---
+++ projetos-de-artigos/usando-middlewares/src/pages/dashboard-2.tsx
@@ -0,0 +1,8 @@
+import styles from "../styles/Dashboard.module.css"
+
+export default function Dashboard2() {
+  return <div className={styles.dashboard}>
+    <h1>Nós fomos redirecionados</h1>
+    <p>Estamos na página dashboard-2</p>
+  </div>;
+}
