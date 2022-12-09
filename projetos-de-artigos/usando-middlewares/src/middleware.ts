d1f7d8d6caf60f6c5fced353ae30ffebe635c0fc
Marlliton Souza <67978032+Marlliton@users.noreply.github.com>
2022-12-09 09:53:10 -0300

    up middleware

--- projetos-de-artigos/usando-middlewares/src/middleware.ts
+++ projetos-de-artigos/usando-middlewares/src/middleware.ts
@@ -1,10 +1,24 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const isAuthenticated = false; // Usuário "não autenticado"
 
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/dashboard-2', request.url))
+  if (!isAuthenticated) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Fala na autenticação",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  return NextResponse.redirect(new URL("/dashboard-2", request.url));
 }
 
 export const config = {
-  matcher: '/dashboard/:path*',
-}
+  matcher: ["/dashboard/:path*", "/dashboard-2/:path*"],
+};
